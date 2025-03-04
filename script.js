e6aae3ce5b1b43a68f2d87aa7e3f4e7f1643a161
Ermrz09 <edwinrmrz09@gmail.com>
2025-03-04 00:05:47 -0600

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-// Show the birthday message when the button is clicked
-document.getElementById('wishButton').onclick = function() {
-    document.getElementById('birthdayMessage').style.display = 'block';
-    document.getElementById('cake').style.display = 'none'; // Hide the cake
-    document.getElementById('cakeGif').style.display = 'block'; // Show the GIF
-};
